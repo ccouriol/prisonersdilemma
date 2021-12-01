404ca9874c1b3173ab4b93a16422a0821453d845
ccouriol <clement.couriol@cpe.fr>
2021-12-01 16:36:37 +0100

    feat: verifyIP util function

--- src/client/utils/utils.c
+++ src/client/utils/utils.c
@@ -11,6 +11,7 @@
  */
 
 /*! Importation of librairies*/
+#include "../../../include/client/utils/utils.h"
 #include "../../../include/client/game/main.h"
 
 /*!
@@ -22,15 +23,12 @@
  * \remarks None
  * \param ptr
  */
-void *threadProcess(void *ptr)
-{
+void *threadProcess(void *ptr) {
   char buffer_in[BUFFERSIZE];
   int sockfd = *((int *)ptr);
   long int len;
-  while ((len = read(sockfd, buffer_in, BUFFERSIZE)) != 0)
-  {
-    if (strncmp(buffer_in, "exit", 4) == 0)
-    {
+  while ((len = read(sockfd, buffer_in, BUFFERSIZE)) != 0) {
+    if (strncmp(buffer_in, "exit", 4) == 0) {
       break;
     }
 
@@ -49,8 +47,7 @@ void *threadProcess(void *ptr)
  * \brief Thread for collecting the id of the game and acknowledge it to the
  * server \remarks None \param ptr
  */
-void *threadIsGame(void *ptr)
-{
+void *threadIsGame(void *ptr) {
   char buffer_in[BUFFERSIZE];
   char buffer_out[BUFFERSIZE];
   char idGame[BUFFERSIZE];
@@ -59,17 +56,14 @@ void *threadIsGame(void *ptr)
   long int len;
   int sockfd = *((int *)ptr);
 
-  while ((len = read(sockfd, buffer_in, BUFFERSIZE)) != 0)
-  {
-    if (recv(sockfd, idGame, BUFFERSIZE, 0) < 0)
-    {
+  while ((len = read(sockfd, buffer_in, BUFFERSIZE)) != 0) {
+    if (recv(sockfd, idGame, BUFFERSIZE, 0) < 0) {
       puts("recv failed");
     }
     puts("replied recieved");
   }
 
-  if (send(sockfd, idGame, strlen(idGame), 0) < 0)
-  {
+  if (send(sockfd, idGame, strlen(idGame), 0) < 0) {
     puts("Send failed");
   }
   puts("Send recieved");
@@ -87,8 +81,7 @@ void *threadIsGame(void *ptr)
  * \remarks None
  * \return
  */
-int open_connection()
-{
+int open_connection() {
   int sockfd;
 
   struct sockaddr_in serverAddr;
@@ -108,11 +101,42 @@ int open_connection()
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
   // Connect the socket to the server using the address
-  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) != 0)
-  {
+  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) !=
+      0) {
     puts("Fail to connect to server");
     exit(-1);
   };
 
   return sockfd;
+}
+
+/*!
+ * \fn int verifyIP(char *string_ip)
+ * \author Clément Couriol
+ * \version 0.1
+ * \date  01/12/2021
+ * \brief Returns 0 if the ip is valid, 1 if not
+ * \remarks None
+ * \param string_ip
+ * \return
+ */
+int verifyIP(char *string_ip) {
+  regex_t regex;
+  int reti;
+  char *rgx = "^(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2["
+              "0-4][0-9]|[01]?[0-9][0-9]?)){3}$";
+
+  reti = regcomp(&regex, rgx, REG_EXTENDED);
+  if (reti)
+    return EXIT_FAILURE;
+
+  reti = regexec(&regex, string_ip, 0, NULL, 0);
+  if (!reti)
+    return EXIT_SUCCESS;
+  else if (reti == REG_NOMATCH)
+    return EXIT_FAILURE;
+  else
+    return EXIT_FAILURE;
+
+  regfree(&regex);
 }
